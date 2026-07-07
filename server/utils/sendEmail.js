3fcee3100fd493a679557eaf9ccbc82cb8267fc1
nilanshmishra05 <nilanshmishra05@gmail.com>
2026-07-07 22:57:27 +0530

    Switch SMTP port to 587 (TLS/STARTTLS) for Render compatibility

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,8 +4,8 @@ const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    port: 587,
+    secure: false, // false for 587, true for 465
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
